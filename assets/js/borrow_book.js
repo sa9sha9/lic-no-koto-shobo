edb734a3fe2dc52d73199ca1d5433e3034766205
sa9sha9 <sha9sa9@gmail.com>
2018-09-16 01:28:19 +0900

    Fix doen't work borrow event

--- assets/js/borrow_book.js
+++ assets/js/borrow_book.js
@@ -1,16 +1,14 @@
 const t = TrelloPowerUp.iframe();
 const Promise = window.TrelloPowerUp.Promise;
 
-const CurrentCardContext = {
-	cardId: null,
-	listId: null,
-}
+// card context
+const context = t.getContext();
 
 /**
  * Borrowボタンをクリックした時のイベント処理
  *
  */
-window.btn_borrow.addEventListener( 'submit', function( event ) {
+window.btn_borrow.addEventListener('click', function( event ) {
 	event.preventDefault();
 	const User = {
 		belonging: window.user_belonging.value,
@@ -18,32 +16,28 @@ window.btn_borrow.addEventListener( 'submit', function( event ) {
 		comment: window.user_comment.value,
 	}
 	const comments = `
-	### 借りた人
-	- 所属: ${User.belonging}
-	- 名前: ${User.name}
-	- ひとこと: ${User.comment}
+### 借りた人
+- 所属: ${User.belonging}
+- 名前: ${User.name}
+- ひとこと: ${User.comment}
 	`
-
-	Trello.get(`/cards/${CurrentCardContext.cardId}/actions/comments`, {
+	Trello.post(`/cards/${context.card}/actions/comments`, {
 		text: comments
 	})
 	.then( (res) => {
 		console.log('successfully borrowed')
+		t.closePopup()
 	})
 	.catch( (errorMsg) => {
-		console.error( errorMsg );
+		console.error( 'Comment Error: ', errorMsg );
 	})
 
 	// putBorrowLog(CurrentCardContext, User)
 } );
 
 
-
+/** Rendering borrowing form */
 t.render( function() {
-	const context = t.getContext();
-	CurrentCardContext.listId = context.list;
-	CurrentCardContext.cardId = context.card;
-
 	Trello.get(`/cards/${context.card}`)
 	.then( (res) => {
 		// 本のタイトルを自動入力
@@ -53,17 +47,6 @@ t.render( function() {
 		t.sizeTo('#borrow_form').done()
 	)
 	.catch( (errorMsg) => {
-		console.error( errorMsg );
+		console.error( 'Failed to fetch book_name: ', errorMsg );
 	})
-// return t.sizeTo( '#search_product' ).done();
-	// return t.sizeTo( '#search_product' ).done();
-	// return t.get( 'card', 'shared', 'estimate' )
-	// // データを取得できたら、表示に反映する
-	// .then( function( estimate ) {
-	// 	window.parts_category_selector.value = estimate;
-	// } )
-	// //
-	// .then( function() {
-	// 	t.sizeTo( '#parts_category' ).done();
-	// } );
 } );
