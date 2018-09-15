5ebfca50e357bca47bbce162461cca32d5512112
sa9sha9 <sha9sa9@gmail.com>
2018-09-15 20:43:38 +0900

    Add Icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 console.log( 'hi' );
 
+const TrelloIcons = {
+	LICNOKOTOSHOBO: 'https://raw.githubusercontent.com/sa9sha9/lic-no-koto-shobo/master/assets/image/lic-no-koto-shobo-icon.png'
+}
+
 window.TrelloPowerUp.initialize( {
 	'card-buttons': function( t, opts ) {
 			return [ {
-				icon    : TrelloIcons.CONTROLLER,
+				icon    : TrelloIcons.LICNOKOTOSHOBO,
 				text    : 'この本を借りる',
 				callback: function( t ) {
 					return t.popup( {
